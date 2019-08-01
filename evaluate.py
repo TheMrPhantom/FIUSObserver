175d4bf72bc71d73f6b5da468047edd61b6905e2
Matthias Hermann <matthias.h@online.de>
2019-08-01 16:31:24 +0200

    Add method for reading observation data

--- evaluate.py
+++ evaluate.py
@@ -2,6 +2,22 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+import csv
+import os
+
+
+def read_observation_data(filename):
+    data = []
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in reader:
+            data.append(row)
+    return data
+
+
+data_filename = os.path.abspath(sys.argv[1])
+read_observation_data(data_filename)
 
 hours_per_day = 24
 days_per_week = 7
